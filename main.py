4ec64b144e7b3f50d48434654effcc452019cc92
James Mwai <smartemwa@gmail.com>
2018-10-25 14:49:57 +0300

    Updated to ke

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 def nearby():
     fields = "photos,formatted_address,name"
-    location = "-26.204103,28.047304"
+    location = "-1.308869,36.812077"
     keyword = "coffee shop"
     key = "AIzaSyAfBOVn3dRaeJ78FgqLNsWFec84oIe4e6A"
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields={}&location={}&radius=150000&keyword={}&key={}"
