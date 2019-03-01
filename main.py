de7ece9efa09b10d85427aea7dbd789ddc2a34e8
James Mwai <smartemwa@gmail.com>
2019-03-01 07:48:06 +0300

    Updated index display

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 def nearby():
     fields = "photos,formatted_address,name"
-    location = "-1.308869,36.812077"
-    keyword = "coffee shop"
+    location = "-1.30316895,36.82606122"
+    keyword = "apartment"
     key = "AIzaSyAfBOVn3dRaeJ78FgqLNsWFec84oIe4e6A"
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields={}&location={}&radius=150000&keyword={}&key={}"
     url = url.format(fields, location, keyword, key)
