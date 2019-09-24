43e1d7673303bc06ac4cb805cdb45359cd24fee6
James Mwai <smartemwa@gmail.com>
2019-09-24 11:50:13 +0300

    Updated flask app, kubernetes manifests and cloudbuild

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 def nearby():
     fields = "photos,formatted_address,name"
-    location = "5.5698567,-0.169646"
-    keyword = "coffee shop"
+    location = "-1.2889978,36.7833679"
+    keyword = "coworking space"
     key = "AIzaSyAfBOVn3dRaeJ78FgqLNsWFec84oIe4e6A"
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?fields={}&location={}&radius=150000&keyword={}&key={}"
     url = url.format(fields, location, keyword, key)
